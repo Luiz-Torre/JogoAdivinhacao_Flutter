32416ac1e6bdd38b74ef1a377ea1a9eeecc07e09
Luiz Torre <69131416+Luiz-Torre@users.noreply.github.com>
2022-10-07 15:42:36 -0300

    Criando o Projeto

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:JogoAdivinhacao_Flutter/pages/home.dart';
+void main() {
+  runApp(
+    const MaterialApp(
+          title: "Jogo de Adivinhacao",
+          home: Home()
+    )
+  );
+  
+}
